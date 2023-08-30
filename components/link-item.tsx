c4148f23ae5267b4399f6aea29c94beeb924de73
Artyom Galay <artemijgalaj@gmail.com>
2023-08-30 22:36:28 +0600

    New Animation

--- components/link-item.tsx
+++ components/link-item.tsx
@@ -1,13 +1,11 @@
 import React, { FC, useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, stagger } from "framer-motion";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import { LinkItemProps } from "@/interfaces/link.interface";
 import imageLoader from "@/imageLoader";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-
-const staggerDelay = 0.1; // Задержка между элементами
 
 const LinkItem: FC<LinkItemProps> = ({
 	title,
@@ -18,20 +16,20 @@ const LinkItem: FC<LinkItemProps> = ({
 	distance,
 }) => {
 	const [active, setActive] = useState<boolean>(false);
+	const staggerItems = stagger(0.1, { startDelay: 0.15 });
 
 	useEffect(() => {
-		const timeout = setTimeout(() => {
+		setTimeout(() => {
 			setActive(true);
-		}, delay);
-
-		return () => clearTimeout(timeout);
-	}, [delay]);
+		}, 400);
+	}, []);
 
 	return (
 		<Link href={path} className="link">
 			<motion.a
 				style={{
-					translate: distance,
+					translate: active ? 0 : distance,
+					opacity: active ? 1 : 0,
 				}}
 				initial={{ opacity: 0, scale: 0.3, filter: "blur(20px)" }}
 				animate={{
@@ -41,15 +39,14 @@ const LinkItem: FC<LinkItemProps> = ({
 				}}
 				transition={{
 					duration: 0.2,
-					delay: active ? delay + staggerDelay : delay,
+					delay: 0.4,
 				}}
-				exit={{ opacity: 0, scale: 0.3, filter: "blur(20px)" }}
 			>
-				<Card className="hover:bg-zinc-800/50 bg-zinc-800/30 backdrop-blur-md px-3 py-5 bg-zinc-800 border-none rounded-xl flex flex-col items-center gap-5">
+				<Card className="hover:bg-zinc-800/50 px-3 py-5 bg-zinc-800 border-none rounded-xl flex flex-col items-center gap-5">
 					<div className="w-full h-full flex flex-col items-center justify-between gap-5">
 						<Image
 							src={imageUrl}
-							alt="Socia Media"
+							alt="Social Media"
 							width={50}
 							height={50}
 							className="w-[50px] h-[50px]"
